e0da9b954ba69485a11562297f5aac372dd9d8a9
Amit Sharma <amit.sharma@vgw.co>
2018-09-04 12:33:34 +0800

    Working example of product templates versioning

--- src/SynthProductTemplates.ts
+++ src/SynthProductTemplates.ts
@@ -1,17 +1,40 @@
 import cdk = require("@aws-cdk/cdk");
 import { MyS3BucketStack } from "./product_definitions/MyS3BucketStack";
 import fs = require("fs-extra");
-
+const PRODUCT_DEFINITION_VERSION_FILE_NAME = "product-definition-versions.json";
 
 const app = new cdk.App(process.argv);
 
-// Generate cloudformation templates (the templates to be attached to catalog products)
-const bucketStackTemplate = new MyS3BucketStack(app, "MyS3BucketStack").toCloudFormation();
+synth();
 
-const templateStr = JSON.stringify(bucketStackTemplate);
-fs.outputFile("product-templates/s3-bucket-product/v1/MyS3BucketStack.template.yaml", templateStr).then(() => {
-    console.log("MyS3BucketStack.template.yaml was written successfully");
-}).catch((err) => {
-    console.error(err);
-});
-console.log("bucketStackTemplate: " + templateStr);
+
+// Functions
+function synth() {
+    const version = getVersion("MyS3BucketStack");
+    const template = generateCfnTemplates();
+    writeToFile(version, template);
+}
+
+function writeToFile(version: string, template: string) {
+    fs.outputFile("product-templates/s3-bucket-product/" + version + "/MyS3BucketStack.template.yaml", template).then(() => {
+        console.log("MyS3BucketStack.template.yaml was written successfully");
+    }).catch((err) => {
+        console.error(err);
+    });
+}
+
+// Generate cloudformation templates (the templates to be attached to catalog products)
+function generateCfnTemplates(): string {
+    const bucketStackTemplate = new MyS3BucketStack(app, "MyS3BucketStack").toCloudFormation();
+    return JSON.stringify(bucketStackTemplate);
+}
+
+function getVersion(stackName: string): string {
+    let version;
+    let property = stackName;
+
+    const result = fs.readJsonSync(PRODUCT_DEFINITION_VERSION_FILE_NAME);
+    const jsonString = JSON.stringify(result);
+    version = JSON.parse(jsonString)[property].version;
+    return version;
+}
